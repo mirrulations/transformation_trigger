ba778dcc81edcd79649087d014b792a177b1c6bc
Zachery Bingaman <zacherybingaman@gmail.com>
2025-03-26 13:00:35 -0400

    fix opensearch secrets and modify common file to use them

--- dev-env/lambda_functions/open_search/app.py
+++ dev-env/lambda_functions/open_search/app.py
@@ -1,52 +1,7 @@
 import json
-import os
-import psycopg
 import boto3
-from botocore.exceptions import ClientError
 from common.ingest import ingest_comment
 
-def get_secret(secret_name):
-   """
-   Retrieve a secret from AWS Secrets Manager
-   Args:
-       secret_name: Name of the secret to retrieve
-   Returns:
-       dict: The secret key/value pairs
-   """
-   region_name = os.environ.get('AWS_REGION', 'us-east-1')
-
-   # Create a Secrets Manager client
-   session = boto3.session.Session()
-   client = session.client(
-       service_name='secretsmanager',
-       region_name=region_name
-   )
-
-   try:
-       # Get the secret value
-       response = client.get_secret_value(SecretId=secret_name)
-
-       # Decode and parse the secret string JSON
-       if 'SecretString' in response:
-           secret = json.loads(response['SecretString'])
-           return secret
-       else:
-           print("Secret not found in expected format")
-           raise Exception("Secret not found in expected format")
-
-   except ClientError as e:
-       print(f"Error retrieving secret: {str(e)}")
-       raise e
-   
-def get_db_connection():
-   """
-   Retrieve a connection to the PostgreSQL database
-   """
-   secret_name = os.environ.get('DB_SECRET_NAME')
-   secret = get_secret(secret_name)
-
-   os.putenv("OPENSEARCH_HOST",secret['host'])
-   os.putenv("OPENSEARCH_PORT",secret['port'])
 
 def handler(event, context):
    """
@@ -75,8 +30,7 @@ def handler(event, context):
            raise ValueError("File content is empty")
 
        if 'comment' in s3dict['file_key']:
-           #set environment variables
-           get_db_connection()
+           #set environment variables and ingest comment
            print("ingesting")
            ingest_comment(file_content)
            print("ingest complete!")
